144c997864c4bd95749df7efd30bf034ee50c453
zishaf <zmturley@gmail.com>
2021-02-25 00:34:07 -0800

    added cost to tile for future pathfinding

--- tile.py
+++ tile.py
@@ -18,6 +18,7 @@ tile_dt = np.dtype(
     [
         ("walkable", np.bool),
         ("transparent", np.bool),
+        ("cost", np.int8),
         ("graphic", graphic_dt),
     ]
 )
@@ -27,37 +28,21 @@ def new_tile(
         *,
         walkable: int,
         transparent: int,
+        cost: int,
         graphic: Tuple[int, Tuple[int, int, int], Tuple[int, int, int]],
 ) -> np.ndarray:
-    return np.array((walkable, transparent, graphic), dtype=tile_dt)
-
+    return np.array((walkable, transparent, cost, graphic), dtype=tile_dt)
 
 floor = new_tile(
     walkable=True,
     transparent=True,
+    cost=1,
     graphic=(46, (150, 150, 150), (30, 30, 30))
 )
 
 wall = new_tile(
     walkable=False,
     transparent=True,
+    cost=0,
     graphic=(127, (116, 94, 162), (20, 20, 20))
 )
-
-
-
-"""my effort at Tile class
-from typing import Tuple
-
-class Tile():
-    def __init__(
-            self, char: int,
-            fg: Tuple[int, int, int]=(255, 255, 255),
-            bg: Tuple[int, int, int]=(0, 0, 0),
-    ):
-        self.char = char
-        self.fg = fg
-        self.bg = bg
-
-WALL  = Tile(127, (169, 153, 255), (20, 20, 20))
-FLOOR = Tile(46, (150, 150, 150), (30, 30, 30))"""
