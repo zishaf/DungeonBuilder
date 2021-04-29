bc661f6a2ddeb982cbac50e22e52b50d7579da2b
zishaf <zmturley@gmail.com>
2021-04-29 03:49:24 -0700

    optimized imports, needs rewrite for new handlers

--- test_functions.py
+++ test_functions.py
@@ -1,17 +1,18 @@
 from __future__ import annotations
-import architect
-import dungeon_features
+
 import random
-import tile
 from typing import TYPE_CHECKING
 
+import architect
+import tile
+
 if TYPE_CHECKING:
-    from event_handler import EventHandler
+    pass
 
 
-def make_egg_map(handler: EventHandler):
+def make_egg_map(floor: architect.Floor):
     #reset the map to all wall
-    architect.reset_map(handler.game_map, denseness=1.0)
+    floor.reset_map(denseness=1.0)
 
     #choose a random starting point in the central third of the map and carve a random rectangle, 4x4 to 10x10
     egg_x = random.randint(int(handler.game_map.width/3), int(handler.game_map.width*2/3))
@@ -19,14 +20,12 @@ def make_egg_map(handler: EventHandler):
     egg_width, egg_height = random.randint(4,10), random.randint(4, 10)
 
     handler.game_map.tiles[egg_x:egg_x+egg_width, egg_y:egg_y+egg_height] = tile.floor
-    handler.message_log.add_message("egg rect done")
     handler.update_console()
 
     #smooth it out a bit
     smoothing_passes = random.randint(3, 8)
     for i in range(smoothing_passes):
         architect.smooth_it_out(handler.game_map, smoothness=7)
-    handler.message_log.add_message("smoothed")
     handler.update_console()
 
     #make some long corridors
@@ -34,7 +33,6 @@ def make_egg_map(handler: EventHandler):
     for i in range(corrs):
         length = random.randint(30, 40)
         architect.random_corridor(handler.game_map, length=length)
-    handler.message_log.add_message("long corrs")
     handler.update_console()
 
     #some medium corridors
@@ -42,7 +40,6 @@ def make_egg_map(handler: EventHandler):
     for i in range(corrs):
         length = random.randint(20, 30)
         architect.random_corridor(handler.game_map, length=length)
-    handler.message_log.add_message("mediums corrs")
     handler.update_console()
 
     #and some short corridors
@@ -50,5 +47,4 @@ def make_egg_map(handler: EventHandler):
     for i in range(corrs):
         length = random.randint(10, 20)
         architect.random_corridor(handler.game_map, length=length)
-    handler.message_log.add_message("short corrs")
     handler.update_console()
