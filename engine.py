c398364e0140b4f62b57617d2d51bae45982bd84
zishaf <zmturley@gmail.com>
2021-04-29 03:44:10 -0700

    currently just rendering console/context, formerly part of EventHandlers

--- engine.py
+++ engine.py
@@ -1,28 +1,16 @@
-#currently out of use
-
-import event_handler
+import architect
 import tcod
-from architect import Floor
-from message_log import MessageLog
-
+from settings import SETTINGS
 
 #TODO add player, nu, and flickering halls
 
+MAP_Y_OFFEST = 3
+MAP_X_OFFSET = 0
+
 class Engine:
 
-    def __init__(self, console: tcod.console.Console):
-        self.log = MessageLog()
+    def __init__(self, context, console: tcod.console.Console):
+        #make a new game map and default event handler
         self.console = console
-        self.gameMap = Floor(console.width-20, console.height)
-        self.eventHandler = event_handler.EventHandler(self)
-        
-    def updateConsole(self):
-        self.console.clear()
-        for x in range(len(self.gameMap.floor)):
-            for y in range(len(self.gameMap.floor[x])):
-
-                tile: Tile = self.gameMap.floor[x, y]
-                self.console.tiles_rgb[x, y] = tile.graphic
-
-        #console.tiles_rgb[0:engine.gameMap.width, 0:engine.gameMap.height] = engine.gameMap.floor[]
-        self.log.render(console=self.console, x = self.gameMap.width+1, y=11, width=19, height=self.gameMap.height-20)
+        self.context = context
+        self.game_map = architect.Floor(console.width - MAP_X_OFFSET, console.height - MAP_Y_OFFEST)
