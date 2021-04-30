50826942168a827bbfe6c7c23e62b04a8e26471e
zishaf <zmturley@gmail.com>
2021-04-29 22:35:19 -0700

    modified perform function and added add_args for custom corridors

--- actions.py
+++ actions.py
@@ -1,4 +1,5 @@
 import architect
+import settings
 from dungeon_features import Floor
 from settings import SETTINGS
 from tcod import event
@@ -10,20 +11,30 @@ class Action():
         self.func = func
         self.args = settings
 
+    # TODO modify messages based on args
     def perform(self, map: Floor):
         params = (map,)
         for arg in self.args:
-            params += (arg.val,)
+            if isinstance(arg, settings.Setting):
+                params += (arg.val,)
+            if isinstance(arg, int):
+                params += (arg, )
         self.func(*params)
 
+    def add_args(self, *args):
+        for arg in args:
+            self.args += (arg,)
+
 ACTIONS = {
-    "Smooth":   Action(event.K_s,"Smoothed walls", architect.smooth_it_out,
-                       SETTINGS["(S)moothness"]),
-    "Fill":     Action(event.K_f, "Filled caverns", architect.fill_caverns,
-                       SETTINGS["(F)ill Radius"]),
-    "Reset":    Action(event.K_r, "Reset map",      architect.reset_map,
-                       SETTINGS["(D)enseness"]),
-    "Corridor": Action(event.K_c, "Made corridor",  architect.random_corridor,
-                       SETTINGS["Corridor (L)ength"], SETTINGS["(B)lobulousness"])
+    "smooth":       Action(event.K_s,"Smoothed walls",  architect.smooth_it_out,
+                           SETTINGS["(S)moothness"]),
+    "fill":         Action(event.K_f, "Filled caverns", architect.fill_caverns,
+                           SETTINGS["(F)ill Radius"]),
+    "reset":        Action(event.K_r, "Reset map",      architect.reset_map,
+                           SETTINGS["(D)enseness"]),
+    "corridor":     Action(event.K_c, "Made corridor",  architect.random_corridor,
+                           SETTINGS["Corridor (L)ength"], SETTINGS["(B)lobulousness"]),
+    "corr_between": Action(event.BUTTON_LEFT, "Custom corridor", architect.corridor_between,
+                            )
     #TODO Add mazes, egg maps
 }
