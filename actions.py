3d1940534ad309eb40afb2af6017fc2589d46069
zishaf <zmturley@gmail.com>
2021-04-29 23:21:20 -0700

    added mazes, remove_args function to remove last n args from action

--- actions.py
+++ actions.py
@@ -11,7 +11,7 @@ class Action():
         self.func = func
         self.args = settings
 
-    # TODO modify messages based on args
+    # TODO modify messages based on args, also mazes are adding messages even when they don't draw
     def perform(self, map: Floor):
         params = (map,)
         for arg in self.args:
@@ -25,6 +25,12 @@ class Action():
         for arg in args:
             self.args += (arg,)
 
+    def remove_args(self, n: int):
+        #convert the tuple args to a list, remove the last n items of it, and convert it back to a tuple for
+        lst = list(self.args)
+        del lst[-n:] # <--- python is pretty cool
+        self.args = tuple(lst)
+
 ACTIONS = {
     "smooth":       Action(event.K_s,"Smoothed walls",  architect.smooth_it_out,
                            SETTINGS["(S)moothness"]),
@@ -35,6 +41,8 @@ ACTIONS = {
     "corridor":     Action(event.K_c, "Made corridor",  architect.random_corridor,
                            SETTINGS["Corridor (L)ength"], SETTINGS["(B)lobulousness"]),
     "corr_between": Action(event.BUTTON_LEFT, "Custom corridor", architect.corridor_between,
-                            )
-    #TODO Add mazes, egg maps
+                            ),
+    "maze":         Action(event.K_e,"Made a maze", architect.make_maze,
+                           SETTINGS["Maze (W)idth"], SETTINGS["Maze (H)eight"])
+    #TODO Add mazes, egg maps, flood fill
 }
