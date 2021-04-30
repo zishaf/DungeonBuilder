64e493a90c01f8804126e701e979a1a94f8b1846
zishaf <zmturley@gmail.com>
2021-04-29 23:23:40 -0700

    added maze width and height

--- settings.py
+++ settings.py
@@ -28,5 +28,7 @@ SETTINGS = {
     "(D)enseness"      : Setting(event.K_d, 0.0, 1.0, 0.5, 0.05),
     "(S)moothness"     : Setting(event.K_s, 0, 9, 5, 1),
     "Corridor (L)ength": Setting(event.K_l, 1, 70, 15, 1),
-    "(F)ill Radius"    : Setting(event.K_f, 0, 10, 2, 1)
+    "(F)ill Radius"    : Setting(event.K_f, 0, 10, 2, 1),
+    "Maze (W)idth"     : Setting(event.K_w, 5, 50, 15, 1),
+    "Maze (H)eight"    : Setting(event.K_h, 5, 50, 15, 1),
 }
