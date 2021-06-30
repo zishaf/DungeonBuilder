a1f296e6e22d1433ce5951afb20b6e4a9a32b34f
zishaf <zmturley@gmail.com>
2021-06-30 12:06:43 -0700

    Egg maps now contain multiple eggs. Added Egg class.

--- test_functions.py
+++ test_functions.py
@@ -14,21 +14,54 @@ from engine import Engine
 if TYPE_CHECKING:
     pass
 
+class Egg():
+    # egg is comprised of center point, width and height radiuses, and how many smoothness passes it will get
+    def __init__(self, x, y, width, height, passes):
+        self.x, self.y, self.width, self.height, self.passes = x, y, width, height, passes
+
+        # the leftmost, etc. points will be derived.  passes/2 is how much size is added after all passes
+        self.left = x - width - (passes / 2)
+        self.right = x + width + (passes / 2)
+        self.top = y + height + (passes / 2)
+        self.bottom = y - height - (passes / 2)
+
+    #TODO sometimes intersecting eggs are still created, investigate
+    def intersects(self, egg: Egg) -> bool:
+        # if left or right is in the middle of the egg and top or bottom is in the middle of the egg it intersects
+        if egg.left <= self.left <= egg.right or egg.left <= self.right <= egg.right:
+            if egg.bottom <= self.top <= egg.top or egg.bottom <= self.bottom <= egg.top:
+                return True
+        return False
 
 def make_egg_map(floor: Floor, eng: Engine, handler: event_handler.EventHandler):
     #reset the map to all wall
     architect.reset_map(floor, denseness=1.0)
 
-    #choose a random starting point in the central third of the map and carve a random rectangle, 4x4 to 10x10
-    egg_x = random.randint(int(floor.width/3), int(floor.width*2/3))
-    egg_y = random.randint(int(floor.height/3), int(floor.height*2/3))
-    egg_width, egg_height = random.randint(4,10), random.randint(4, 10)
+    #we want between 1 and 3 eggs, initialize the blank list of them
+    egg_count = random.randint(1, 4)
+    eggs = []
 
-    floor.tiles[egg_x:egg_x+egg_width, egg_y:egg_y+egg_height] = tile.floor
+    #randomly determine how many times we'll smooth it out
+    smoothing_passes = random.randint(3, 8)
+
+
+    while len(eggs) < egg_count:
+        #try a random starting point, width, and height in the central area
+        egg_x = random.randint(int(floor.width / 4), int(floor.width * 3 / 4))
+        egg_y = random.randint(int(floor.height / 4), int(floor.height * 3 / 4))
+        egg_width, egg_height = random.randint(2, 5), random.randint(2, 5)
+
+        new_egg = Egg(egg_x, egg_y, egg_width, egg_height, smoothing_passes)
+        #compare corners against corners of other eggs to avoid intersection
+        if not any(egg.intersects(new_egg) for egg in eggs):
+            eggs.append(new_egg)
+
+    #carve out initial rectangles for the eggs
+    for egg in eggs:
+        floor.tiles[egg.x-egg.width:egg.x+egg.width, egg.y-egg_height:egg.y+egg.height] = tile.floor
     render_and_sleep(.15, eng, handler)
 
     #smooth it out a bit
-    smoothing_passes = random.randint(3, 8)
     for i in range(smoothing_passes):
         architect.smooth_it_out(floor, smoothness=7)
         render_and_sleep(.15, eng, handler)
@@ -38,21 +71,21 @@ def make_egg_map(floor: Floor, eng: Engine, handler: event_handler.EventHandler)
     for i in range(corrs):
         length = random.randint(30, 40)
         architect.random_corridor(floor, length=length)
-        render_and_sleep(.07, eng, handler)
+        render_and_sleep(.05, eng, handler)
 
     #some medium corridors
     corrs = random.randint(8, 14)
     for i in range(corrs):
         length = random.randint(20, 30)
         architect.random_corridor(floor, length=length)
-        render_and_sleep(.05, eng, handler)
+        render_and_sleep(.03, eng, handler)
 
     #and some short corridors
-    corrs = random.randint(50, 60)
+    corrs = random.randint(30, 40)
     for i in range(corrs):
         length = random.randint(10, 20)
         architect.random_corridor(floor, length=length)
-        render_and_sleep(.03, eng, handler)
+        render_and_sleep(.01, eng, handler)
 
 def random_floor(floor: Floor):
     #reset floor with denseness between .2 and .8
@@ -78,12 +111,10 @@ def random_floor(floor: Floor):
                 searching = False
 
         #50/50 of making pyramid maze vs perfect maze
-        if random.random() <.5:
-            maze = PerfectMaze(maze_width, maze_height)
-            floor.tiles[maze_x:maze_x + maze_width, maze_y:maze_y + maze_height] = maze.tiles
-        else:
-            maze = PyramidMaze(maze_width, maze_height)
-            floor.tiles[maze_x:maze_x + maze_width, maze_y:maze_y + maze_height] = maze.tiles
+        maze = PerfectMaze(maze_width, maze_height) if random.random() <.5 else PyramidMaze(maze_width, maze_height)
+
+        floor.tiles[maze_x:maze_x + maze_width, maze_y:maze_y + maze_height] = maze.tiles
+
 
 
 #renders the floor then waits for t seconds
