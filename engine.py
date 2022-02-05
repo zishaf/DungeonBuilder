92f9e8d772a58f368750d1dec40be20b96ad6d5d
zishaf <zmturley@gmail.com>
2022-02-05 13:57:05 -0800

    can now update player fov

--- engine.py
+++ engine.py
@@ -1,6 +1,9 @@
-import architect
+from architect import Floor
 import tcod
 from message_log import MessageLog
+from entity_maker import Player
+from tcod.map import compute_fov
+import numpy as np
 
 #TODO add player, nu, and flickering halls
 
@@ -14,4 +17,29 @@ class Engine:
         self.log = MessageLog()
         self.console = console
         self.context = context
-        self.game_map = architect.Floor(console.width - MAP_X_OFFSET, console.height - MAP_Y_OFFEST)
+        self.game_map = Floor(console.width - MAP_X_OFFSET, console.height - MAP_Y_OFFEST)
+        self.player = Player(self.game_map)
+        self.game_map.entities.append(self.player)
+
+    def update_fov(self) -> None:
+        self.game_map.visible[:] = compute_fov(
+            self.transparent_tiles(),
+            (self.player.x, self.player.y),
+            radius=8,
+            light_walls=True,
+            algorithm=tcod.FOV_DIAMOND
+        )
+
+        self.game_map.explored |= self.game_map.visible
+
+    def transparent_tiles(self) -> np.ndarray:
+        transparent = np.ones((self.game_map.width, self.game_map.height), order="F") \
+            if self.player.flags["see_through_walls"] else self.game_map.tiles["transparent"]
+
+        if self.player.flags["one_eyed"]:
+            new_transparent = np.zeros((self.game_map.width, self.game_map.height), order="F")
+            new_transparent[:, self.player.y] = transparent[:, self.player.y]
+            new_transparent[self.player.x, :] = transparent[self.player.x, :]
+            transparent = new_transparent
+
+        return transparent
