1b8df6f0e1629482ef3b6c576645fb7a2c5955a6
Z <zmturley@gmail.com>
2022-05-21 01:37:22 -0400

    change smoothness settings to work with new fill algorithm

--- settings.py
+++ settings.py
@@ -20,7 +20,7 @@ class Setting:
 SETTINGS = {
     "(B)lobulousness"  : Setting(event.K_b, 0, 3, 0, 1),
     "(D)enseness"      : Setting(event.K_d, 0.0, 1.0, 0.5, 0.05),
-    "(S)moothness"     : Setting(event.K_s, 0, 10, 4, 1),
+    "(S)moothness"     : Setting(event.K_s, 0, 10, 5, 1),
     "Corridor (L)ength": Setting(event.K_l, 1, 70, 15, 1),
     "(F)ill Radius"    : Setting(event.K_f, 0, 10, 2, 1),
     "Maze (W)idth"     : Setting(event.K_w, 5, 60, 20, 1),
